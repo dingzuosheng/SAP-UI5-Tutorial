8b90d8a1e575017004f2439d06a7f17d0e5964dd
ding <zuosheng.ding@adesso.de>
2026-02-09 22:50:05 +0100

    add fragment

--- hello-ding/webapp/controller/HelloPanel.controller.js
+++ hello-ding/webapp/controller/HelloPanel.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment"
 ], 
-    function(Controller, MessageToast) {
+    function(Controller, MessageToast, Fragment) {
         "use strict";
 
         return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
@@ -15,6 +16,23 @@ sap.ui.define([
                 //Show message
                 MessageToast.show(sMsg)
 
+            },
+            onOpenDialog: function () {
+                var oView = this.getView();
+                // create dialog lazily
+                if (!this.byId("helloDialog")) {
+                    // load asynchronous XML fragment
+                    Fragment.load({
+                        id: oView.getId(),
+                        name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                    }).then(function(oDialog) {
+                        //connect the dialog to the root view of this component (models, lifecycle)
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    })
+                } else {
+                    this.byId("helloDialog").open();
+                }
             }
         });
     }
