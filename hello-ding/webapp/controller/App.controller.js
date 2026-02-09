2f140b1bd13086ac76aab52da0d9582062cad748
ding <zuosheng.ding@adesso.de>
2026-02-09 22:29:05 +0100

    add HelloPanel controller and view in capesul

--- hello-ding/webapp/controller/App.controller.js
+++ hello-ding/webapp/controller/App.controller.js
@@ -1,21 +1,9 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller", 
-    "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast",
-    "sap/ui/model/resource/ResourceModel"
 ],
-     function(Controller, JSONModel, MessageToast, ResourceModel){
+     function(Controller, ){
         "use strict";
         return Controller.extend("sap.ui.demo.walkthrough.App", {
-            onShowHello: function () {
-                //read msg from i18n model
-                var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-                //Show message
-                MessageToast.show(sMsg)
-
-            }
+            
         });
 });
